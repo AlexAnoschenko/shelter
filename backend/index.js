f2472ed0026954edf1a4f617fbaa63588c89b093
Aliaksei <=>
2022-04-04 15:39:20 +0300

    S-00009 refactoring

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
 const app = express();
 const WSServer = require('express-ws')(app);
 const aWss = WSServer.getWss();
-const cors = require('cors');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
 const roomsRouters = require('./routers/roomsRouter');
 const cardsRouters = require('./routers/cardsRouter');
 const Room = require('./models/room');
 const Card = require('./models/card');
-
-function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-}
+const {
+  connectionHandler,
+  addUserHandler,
+  openCardHandler,
+} = require('./utils/sockets');
 
 dotenv.config();
 
@@ -29,90 +28,19 @@ app.ws('/', (ws, req) => {
 
     switch (msg.method) {
       case 'connection':
-        connectionHandler(ws, msg);
+        connectionHandler(ws, msg, aWss);
         break;
 
-      case 'updateRoom':
-        Room.findById(msg.id).then(async (room) => {
-          room.users.push(msg.user);
-          room.save();
-
-          if (room.users.length === room.numberOfPlayers) {
-            const cards = await Card.find({});
-
-            room.users.forEach((user, userIndex) => {
-              cards[0].cards.forEach((type, cardsIndex) => {
-                for (let key in type) {
-                  shuffle(type[key]);
-                  room.users[userIndex].cards.push(type[key].shift());
-                }
-              });
-            });
-
-            await Room.findOneAndUpdate(
-              { _id: msg.id },
-              {
-                $set: {
-                  users: room.users,
-                },
-              }
-            ).clone();
-          }
-
-          ws.id = msg.id;
-          aWss.clients.forEach((client) => {
-            if (client.id === msg.id) {
-              client.send(JSON.stringify(room));
-            }
-          });
-        });
+      case 'addUser':
+        addUserHandler(ws, msg, aWss);
         break;
 
       case 'openCard':
-        Room.findById(msg.id).then(async (room) => {
-          room.users.forEach((user) => {
-            if (msg.user.userId === user.userId) {
-              user.cards.forEach(async (card) => {
-                if (String(card.id) === msg.user.card.id) {
-                  card.isVisible = true;
-
-                  await Room.findOneAndUpdate(
-                    { _id: msg.id },
-                    {
-                      $set: {
-                        users: room.users,
-                      },
-                    }
-                  ).clone();
-                }
-              });
-            }
-          });
-
-          ws.id = msg.id;
-          aWss.clients.forEach((client) => {
-            if (client.id === msg.id) {
-              client.send(JSON.stringify(room));
-            }
-          });
-        });
+        openCardHandler(ws, msg, aWss);
         break;
     }
   });
 });
-
-const connectionHandler = (ws, msg) => {
-  ws.id = msg.id;
-  broadcastConnection(ws, msg);
-};
-
-const broadcastConnection = (ws, msg) => {
-  aWss.clients.forEach((client) => {
-    if (client.id === msg.id) {
-      client.send(JSON.stringify(`user ${msg.nickname} connected`));
-    }
-  });
-};
 
 app.use(cors());
 app.use(express.json());
--- frontend/src/pages/CreateRoomPage.jsx/CreateRoomPage.jsx
+++ frontend/src/pages/CreateRoomPage.jsx/CreateRoomPage.jsx
@@ -1,13 +1,11 @@
-import { useHistory } from 'react-router-dom';
 import { useFormik } from 'formik';
 import { makeStyles } from '@mui/styles';
-import { useDispatch, useSelector } from 'react-redux';
 
 import CustomButton from '../../components/Button/Button';
 import CustomTextField from '../../components/TextField/TextField';
 import { CreateRoomSchema } from './validators';
 import { createRoom } from '../../api/room';
-import { addUserAction } from '../../store/actions/roomActions';
+import { useCreateRoomPage } from './hooks';
 
 const useStyles = makeStyles(() => ({
   main: {
@@ -27,25 +25,7 @@ const useStyles = makeStyles(() => ({
 
 const CreateRoomPage = () => {
   const classes = useStyles();
-  const { socket } = useSelector((state) => state.room);
-  const dispatch = useDispatch();
-  const router = useHistory();
-
-  const addUser = async (res) => {
-    dispatch(addUserAction(res));
-  };
-
-  const goToLobbyPage = () => {
-    socket.send(
-      JSON.stringify({
-        method: 'connection',
-        nickname: localStorage.getItem('nickname'),
-        id: localStorage.getItem('roomId'),
-      })
-    );
-
-    router.push(`/lobbyPage/${localStorage.getItem('roomId')}`);
-  };
+  const { addUserStore, goToLobbyPage } = useCreateRoomPage();
 
   const formik = useFormik({
     initialValues: {
@@ -57,7 +37,7 @@ const CreateRoomPage = () => {
         nickname: values.nickname,
         numberOfPlayers: values.numberOfPlayers,
       });
-      addUser(res.data.user);
+      addUserStore(res.data.user);
       goToLobbyPage();
     },
     validationSchema: CreateRoomSchema,
